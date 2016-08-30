d7e2a2635ba72a7c07100e176876cc557c3a0c76
Rebecca Elliff <rme9@students.uwf.edu>
2016-08-30 15:15:35 -0500

    myShell.c

---
+++ myShell.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	char *buffer = NULL;
+	
+	printf("Please Enter Command: ");
+
+	scanf("%s", buffer);
+	
+	if(buffer != NULL)//Exit condition
+	{
+		return 0;
+	}
+	else
+	{
+		//Launch
+	}
+	
+
+
+	return 0; 
+}
